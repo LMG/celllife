d36d2cc3199c1bebfad0a1ef30513e3b7d37ea1b
Louis-Marie Givel <louismariegivel@gmail.com>
2012-12-12 12:01:05 +0100

    Views and all that stuff

--- src/CellLife.java
+++ src/CellLife.java
@@ -1,12 +1,36 @@
 
-public class CellLife {
-
+public class CellLife 
+{
+	static Terminal theTerminal;
+	static View theView;
+	static World theWorld;
+	
+	public CellLife()
+	{
+		System.out.println("Création des vues.");
+		theTerminal = new Terminal();
+		theView = new View();
+		theView.start();
+		
+		System.out.println("Création du monde.");
+		theWorld = new World();
+		theWorld.start();
+		
+		while(true){
+			try
+			{
+				Thread.sleep(10);
+			}
+			catch(InterruptedException ex) {System.out.println("?");}
+		}
+	}
+	
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("It's ALIVE");
-
+	public static void main(String[] args) 
+	{
+		new CellLife();
 	}
 
 }
