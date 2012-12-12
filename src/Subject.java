efd8150d0b00be85f851fb953c1ae7fd6937f276
Vincent Gonse <vincent.gonse@gmail.com>
2012-12-12 10:14:29 +0100

    Creation de la classe Subject

---
+++ src/Subject.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author gonsevi
+ * @since 12/12/12
+ * 
+ */
+
+public abstract class Subject 
+{
+    public void play(){}
+    
+    protected void reproduce(){}
+    
+    protected void move(){}
+    
+    protected void die(){}
+    
+    protected void eat(){}  
+}
