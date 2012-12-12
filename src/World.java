da115ec157956bc68bcf813846d7c16dfefd6058
Antoine Dupre <dupre.antoine.nicolas@gmail.com>
2012-12-12 13:47:37 +0100

    Creation de la matrice de Cellule

--- src/World.java
+++ src/World.java
@@ -1,5 +1,25 @@
 
 public class World extends Thread{
+	
+	public final int LENGTHT = 30 ;
+	public final int WIDTH= 30 ;
+	
+	
+	private Cell cellTab [][] ;
+	
+	
+	public void init()
+	{
+		for(int i=0; i < this.LENGTHT; i++)
+		{
+			for (int j = 0 ; j < this.WIDTH ; j++)
+			{
+				cellTab[i][j] = new Cell(i,j);
+			}
+		}
+	}
+	
+	
 	public void run()
 	{
 		while(true)
@@ -7,6 +27,7 @@ public class World extends Thread{
 			try
 			{
 				Thread.sleep(10);
+				
 			}
 			catch(InterruptedException ex) {System.out.println("?");}
 		}
