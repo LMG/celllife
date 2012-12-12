4e09e1291dbce0cae1e0af2867e1fab4136b6228
Vincent Gonse <vincent.gonse@gmail.com>
2012-12-12 10:39:10 +0100

    Toutes methodes et attributs Subject

--- src/Subject.java
+++ src/Subject.java
@@ -7,6 +7,55 @@
 
 public abstract class Subject 
 {
+    /******************** Attributs ********************/
+    
+    private int energy;
+    private Cell position;
+    
+    
+    /******************** Constructeurs ********************/
+  
+    public Subject ()
+    {
+        this.energy = 100;
+    }
+    
+    public Subject (Cell position)
+    {
+        this.energy = 100;
+        this.position = position;
+    }
+    
+    public Subject (Cell position, int energy)
+    {
+        this.energy = energy;
+        this.position = position;
+    }
+    
+    /******************** Accesseurs ********************/
+    
+    public void setEnergy (int energy)
+    {
+        this.energy = energy;
+    }
+    
+    public int getEnergy ()
+    {
+        return this.energy;
+    }
+    
+    public void setPosition(Cell position)
+    {
+        this.position = position;
+    }
+    
+    public Cell getPosition()
+    {
+        return this.position;
+    }
+    
+    /******************** Méthodes ********************/
+    
     public void play(){}
     
     protected void reproduce(){}
@@ -15,5 +64,8 @@ public abstract class Subject
     
     protected void die(){}
     
-    protected void eat(){}  
+    protected void eat()
+    {
+        this.energy = 
+    }  
 }
