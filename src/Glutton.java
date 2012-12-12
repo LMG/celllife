b0ffe35f89ea2caa4a07828dd8cea9d273f69f95
Louis-Marie Givel <louismariegivel@gmail.com>
2012-12-12 14:23:29 +0100

    Classe glutton bidon

---
+++ src/Glutton.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * @author Louis-Marie Givel
+ */
+public class Glutton extends Subject{
+    
+    /******************** Méthodes ********************/
+    
+    protected Cell direction()
+    {
+        return this.position; //A modifier
+    }
+    
+    protected void eat()
+    {   
+        //A completer
+    }  
+    
+}
