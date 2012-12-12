a57298b09d8a60ffb8db89c212964d7cfe9eb1b5
Antoine Dupre <dupre.antoine.nicolas@gmail.com>
2012-12-12 14:15:00 +0100

    Initialisation des Erratic

--- src/World.java
+++ src/World.java
@@ -1,23 +1,45 @@
 
 public class World extends Thread{
 	
-	public final int HIGHT = 30 ;
-	public final int WIDTH= 30 ;
+	public static final int HEIGHT = 30 ;
+	public static final int WIDTH= 30 ;
 	
+	public static final int NUMBER_RABBIT = 10;
+	public static final int NUMBER_CANNIBAL = 10;
+	public static final int NUMBER_ERRATIC = 10;
+	public static final int NUMBER_GLUTTON = 10;
 	
 	private Cell cellTab [][] ;
 	
 	
 	public void init()
 	{
-		for(int i=0; i < this.HIGHT; i++)
+	int numberRabbit = NUMBER_RABBIT;
+	int numberCannibal = NUMBER_CANNIBAL;
+	int numberErratic = NUMBER_ERRATIC;
+	int numberGlutton = NUMBER_GLUTTON;
+		
+	int randHeigth;
+	int randWidth;
+	
+		for(int i=0; i < HEIGHT; i++)
 		{
-			for (int j = 0 ; j < this.WIDTH ; j++)
+			for (int j = 0 ; j < WIDTH ; j++)
 			{
 				cellTab[i][j] = new Cell(i,j);
 			}
 		}
+		
+		//placement des erratic
+		while(numberErratic != 0)
+		{
+			randHeigth = ((int) Math.random()) % HEIGHT;
+			randWidth = ((int) Math.random()) % WIDTH;
+			cellTab[randHeigth][randWidth].getSubjects().add( new Erratic(cellTab[randHeigth][randWidth], 100));
+			numberErratic--;
+		}
 	}
+	
 	
 	
 	public void run()
