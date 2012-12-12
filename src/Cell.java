714c1ece99ff38f1c3b716b17b26afd071f4c100
Antoine Dupre <dupre.antoine.nicolas@gmail.com>
2012-12-12 13:48:08 +0100

    modification du constructeur

--- src/Cell.java
+++ src/Cell.java
@@ -5,20 +5,32 @@ import java.util.ArrayList;
  */
 public class Cell 
 {
-	public final int x= 30;
-	public final int y= 30;
+	public final int x;
+	public final int y;
 	
 	private int vegetables;
 	private int meat;
 	
 	private ArrayList<Subject> subjects;
-	public Cell()
+	
+	
+	public Cell(int x , int y)
 	{
 		super();	
+		this.x = x;
+		this.y= y;
 		this.vegetables=0;
 		this.meat=0;
 	}
 	
+	public Cell(int x , int y , int meat , int vegetagles)
+	{
+		super();
+		this.x = x;
+		this.y= y;
+		this.vegetables=vegetagles;
+		this.meat=meat;
+	}
 	
 	public ArrayList<Subject> getSubjects()
 	{
