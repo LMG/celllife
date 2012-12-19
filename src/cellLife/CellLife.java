29916b06ae3febbec42554b62fd7216fca7a1f02
Louis-Marie Givel <louismariegivel@gmail.com>
2012-12-19 10:04:08 +0100

    Mise aux normes cellLife.java

--- src/cellLife/CellLife.java
+++ src/cellLife/CellLife.java
@@ -1,31 +1,50 @@
 package cellLife;
 
-public class CellLife 
+/**
+ *
+ */
+public final class CellLife
 {
-	static Terminal theTerminal;
-	static View theView;
-	static World theWorld;
-	
-	public CellLife()
-	{
-		System.out.println("Création du monde.");
-		theWorld = new World(theView);
-		theWorld.start();
+    /**
+     * @param args
+     */
+    private static Terminal theTerminal;
+    /**
+     * @param args
+     */
+    private static View theView;
+    /**
+     * @param args
+     */
+    private static World theWorld;
 
-		System.out.println("Création des vues.");
-		theTerminal = new Terminal(theWorld);
-		theView = new View(theWorld);
-		
-		theTerminal.start();
-		theView.start();
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) 
-	{
-		new CellLife();
-	}
+    /**
+     * @param args
+     */
+    private CellLife()
+    {
+        System.out.println("Création du monde.");
+        theWorld = new World(theView);
+        theWorld.start();
+
+        System.out.println("Création des vues.");
+        theTerminal = new Terminal(theWorld);
+        theView = new View(theWorld);
+
+        theTerminal.start();
+        theView.start();
+    }
+
+    /** Simulation start.
+     * 
+     * @param args arguments
+     */
+    //On autorise cette méthode principale: c'est le point d'entrée du programme
+    //CHECKSTYLE:OFF
+    public static void main(final String[] args)
+    //CHECKSTYLE:ON
+    {
+        new CellLife();
+    }
 
 }
